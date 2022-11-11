8eebac10b8a78f40aabf0245fc200969d0febe10
drfein <82621820+drfein@users.noreply.github.com>
2022-11-10 23:43:59 -0800

    Fixed onboarding bug

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -8,54 +8,40 @@ export default function OnboardingScreen({navigation}) {
     const [number, onChangeNumber] = useState(null)
 
     return (
-      <View style={styles.container}>
-        <View style={styles.progressBar}>
-            <View style={{...StyleSheet.absoluteFill, backgroundColor: colors.budder, width: '25%'}}/>
+        <View style={styles.container}>
+          <View style={styles.progressBar}>
+              <View style={{...StyleSheet.absoluteFill, backgroundColor: colors.budder, width: '25%'}}/>
+          </View>
+          <Text style={styles.header}>WHAT IS YOUR FULL NAME?</Text>
+          
+          <LinearGradient 
+              colors={username ? [colors.budder, colors.maroon] : ["#606060", "#606060"]}
+              style={styles.inputGrad}
+              start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}>
+              <TextInput style={styles.inputFilled} onChangeText={onChangeName} value={username} placeholder="Your Name"/>
+          </LinearGradient>
+  
+          <Text style={styles.header}>PHONE NUMBER</Text>
+  
+          <LinearGradient 
+              colors={number ? [colors.budder, colors.maroon] : ["#606060", "#606060"]}
+              style={styles.inputGrad}
+              start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}>
+              <TextInput style={styles.inputFilled} onChangeText={onChangeNumber} value={number} placeholder="Phone Number"/>
+          </LinearGradient>
+  
+          <LinearGradient 
+              style={styles.nextButton}
+              colors={(number && username) ? [colors.budder, colors.maroon] : ["#606060", "#606060"]}
+              start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}
+              location={[0, 0.8]}>
+              <Pressable style={styles.nextButtonFilled} onPress={() => navigation.navigate("Interests")}>
+                  <Image source={require('../assets/Images/arrow-right.png')}/>
+              </Pressable>
+          </LinearGradient>
+          
         </View>
-        <Text style={styles.header}>WHAT IS YOUR FULL NAME?</Text>
-        
-        {username ? (
-            <LinearGradient 
-                colors={[colors.budder, colors.maroon]}
-                style={styles.inputGrad}
-                start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}>
-                <TextInput style={styles.inputFilled} onChangeText={onChangeName} value={username} placeholder="Your Name"/>
-            </LinearGradient>
-        ) : (
-            <TextInput style={styles.input} onChangeText={onChangeName} value={username} placeholder="Your Name"/>
-        )}
-
-        <Text style={styles.header}>PHONE NUMBER</Text>
-        {number ? (
-            <LinearGradient 
-                colors={[colors.budder, colors.maroon]}
-                style={styles.inputGrad}
-                start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}>
-                <TextInput style={styles.inputFilled} onChangeText={onChangeNumber} value={number} placeholder="Phone Number"/>
-            </LinearGradient>
-        ) : (
-            <TextInput style={styles.input} onChangeText={onChangeNumber} value={number} placeholder="Phone Number"/>
-        )}
-
-        {(number && username) ? (
-            <LinearGradient 
-                style={styles.nextButton}
-                colors={[colors.budder, colors.maroon]}
-                start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}
-                location={[0, 0.8]}>
-                <Pressable style={styles.nextButtonFilled} onPress={() => navigation.navigate("Interests")}>
-                    <Image source={require('../assets/Images/arrow-right.png')}/>
-                </Pressable>
-            </LinearGradient>
-            
-        ) : (
-            <Pressable style={styles.nextButton}>
-                <Image source={require('../assets/Images/arrow-right.png')}/>
-            </Pressable>
-        )}
-        
-      </View>
-    );
+      );
   }
 
   const styles = StyleSheet.create({
