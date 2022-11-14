97c725e523bcdd656fbe808fdb5cfa180c46aff3
Karina Li <104478165+karinakli@users.noreply.github.com>
2022-11-14 15:50:04 -0800

    splash screen

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StyleSheet, Text, View, Image, useState, Pressable } from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { NavigationContainer, StackActions, useNavigationContainerRef } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState, useEffect, useCallback } from 'react';
 
 import {colors} from './assets/Themes/colors'
 import {useFonts} from 'expo-font'
 
-import SplashScreen from './screens/SplashScreen';
+import * as SplashScreen from 'expo-splash-screen';
 import LogInScreen from './screens/LogInScreen';
 import OnboardingScreen from './screens/OnboardingScreen';
 import InterestsScreen from './screens/InterestsScreen';
 
-
+// Keep the splash screen visible while we fetch resources
+// SplashScreen.preventAutoHideAsync();
 const Stack = createStackNavigator();
 
-
 export default function App() {
+  // const [logIn, setLogIn] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded] = useFonts({
     'Inter-Bold': require('./assets/Fonts/Inter-Bold.ttf'),
     'Inter-Light': require('./assets/Fonts/Inter-Light.ttf'),
@@ -24,16 +28,20 @@ export default function App() {
     'FredokaOne': require('./assets/Fonts/FredokaOne.ttf')
   });
 
+  if (!fontsLoaded && !appIsReady) {
+    return <AppLoading />;
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen name="Splash" component={SplashScreen} options={{headerShown: false}}/> */}
-        <Stack.Screen name="Login" component={LogInScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Interests" component={InterestsScreen} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    // <View style={styles.container} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LogInScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Interests" component={InterestsScreen} options={{headerShown: false}}/>
+          </Stack.Navigator>
+      </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
