e01cc7907f0ac8358fb702439dc8b36d211c26c1
drfein <82621820+drfein@users.noreply.github.com>
2022-12-05 06:31:45 -0800

    Finish first itinerary page

--- screens/ItineraryScreen.js
+++ screens/ItineraryScreen.js
@@ -1,4 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
+import { AutoFocus } from 'expo-camera';
 import {LinearGradient} from 'expo-linear-gradient';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView,Dimensions } from 'react-native';
@@ -7,8 +8,9 @@ import {colors} from '../assets/Themes/colors'
 const windowWidth = Dimensions.get('window').width;
 
 export default function ItineraryScreen({navigation}) {
-
+    const [locationSelection, setLocationSelection] = useState("");
     const [friendSelection, setFriendSelection] = useState("");
+    const [planSelection, setPlanSelection] = useState("");
   
     return (
       <View style={styles.container}>
@@ -33,6 +35,77 @@ export default function ItineraryScreen({navigation}) {
               </TouchableOpacity>
             </View>
           </LinearGradient>
+        </View>
+        {friendSelection !== "" ? (
+        <View style={styles.selectFriendContainer}>
+          <Text style={styles.subheaderText} >somwhere...</Text>
+          <LinearGradient 
+              colors={[colors.budder, colors.maroon]}
+              style={styles.gradientContainer}
+              start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}>
+            <View style={styles.selectionContainer}>
+              <TouchableOpacity style={ (locationSelection === "SF" ) ? styles.buttonSelected : styles.button} onPress={() => setLocationSelection("SF")}>
+                <Text style={styles.buttonText}>in SF</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={ (locationSelection === "SC" ) ? styles.buttonSelected : styles.button} onPress={() => setLocationSelection("SC")}>
+                <Text style={styles.buttonText}>in Santa Clara County</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={ (locationSelection === "5 miles") ? styles.buttonSelected : styles.bottomButton} onPress={() => setLocationSelection("5 miles")}>
+                <Text style={styles.buttonText}>within 5 miles</Text>
+              </TouchableOpacity>
+            </View>
+          </LinearGradient>
+        </View>
+        ) : null}
+        {locationSelection !== "" ? (
+        <View style={styles.selectFriendContainer}>
+          <Text style={styles.subheaderText} >and I'm in the mood for...</Text>
+          <LinearGradient 
+              colors={[colors.budder, colors.maroon]}
+              style={styles.gradientContainer}
+              start={{x:0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}>
+            <View style={styles.selectionContainer}>
+              <TouchableOpacity style={ (planSelection === "a study date" ) ? styles.buttonSelected : styles.button} onPress={() => setPlanSelection("a study date")}>
+                <Text style={styles.buttonText}>a study date</Text>
+                <Ionicons name="school-outline" size={24} color={colors.rust} />
+              </TouchableOpacity>
+              <TouchableOpacity style={ (planSelection === "brunch" ) ? styles.buttonSelected : styles.button} onPress={() => setPlanSelection("brunch")}>
+                <Text style={styles.buttonText}>brunch</Text>
+                <Ionicons name="cafe-outline" size={24} color={colors.rust} />
+              </TouchableOpacity>
+              <TouchableOpacity style={ (planSelection === "an adventure" ) ? styles.buttonSelected : styles.button} onPress={() => setPlanSelection("an adventure")}>
+                <Text style={styles.buttonText}>an adventure</Text>
+                <Ionicons name="map-outline" size={24} color={colors.rust} />
+              </TouchableOpacity>
+              <TouchableOpacity style={ (planSelection === "something artsy" ) ? styles.buttonSelected : styles.bottomButton} onPress={() => setPlanSelection("something artsy")}>
+                <Text style={styles.buttonText}>something artsy</Text>
+                <Ionicons name="color-palette-outline" size={24} color={colors.rust} />
+              </TouchableOpacity>
+            </View>
+          </LinearGradient>
+        </View>
+        ) : null}
+        <View style={styles.buttonContainer}>
+            {friendSelection === "" ? (
+            <View style={styles.outlinedButton}>
+              <Text style={styles.yellowButtonText}>0 of 3</Text>
+            </View>
+            ) : null }
+            {friendSelection !== "" && locationSelection === "" ? (
+            <View style={styles.outlinedButton}>
+              <Text style={styles.yellowButtonText}>1 of 3</Text>
+            </View>
+            ) : null }
+            { locationSelection !== "" && planSelection === "" ? (
+            <View style={styles.outlinedButton}>
+              <Text style={styles.yellowButtonText}>2 of 3</Text>
+            </View>
+            ) : null }
+            {planSelection !== "" ? (
+            <TouchableOpacity style={styles.yellowButton}>
+              <Text style={styles.yellowButtonText}>Find a plan</Text>
+            </TouchableOpacity>
+            ) : null }
         </View>
 
       </View>
@@ -96,17 +169,20 @@ export default function ItineraryScreen({navigation}) {
       flexDirection: 'row',
       borderBottomColor: colors.lightGray,
       borderBottomWidth: 1,
+      justifyContent: 'space-between',
       width: '100%',
     },
     buttonSelected: {
       padding: 10,
       flexDirection: 'row',
+      justifyContent: 'space-between',
       backgroundColor: colors.budder,
       width: '100%',
     },
     bottomButton: {
       padding: 10,
       flexDirection: 'row',
+      justifyContent: 'space-between',
       width: '100%',
     },
     buttonText: {
@@ -114,6 +190,22 @@ export default function ItineraryScreen({navigation}) {
       fontFamily: 'Inter-Regular',
       color: colors.rust,
       textAlign: 'center'
+    },
+    buttonContainer: {
+      position: 'absolute',
+      bottom: 10,
+      width: '100%',
+      alignItems: 'center'
+    },
+    outlinedButton: {
+      backgroundColor: colors.white,
+      padding: 10,
+      width: 250,
+      borderRadius: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: colors.rust,
+      borderWidth: 2,
     },
     yellowButton: {
       backgroundColor: colors.budder,
@@ -123,5 +215,11 @@ export default function ItineraryScreen({navigation}) {
       justifyContent: 'center',
       alignItems: 'center',
     },
+    yellowButtonText: {
+      fontSize: 16,
+      fontFamily: 'Inter-Bold',
+      color: colors.rust,
+      textAlign: 'center'
+    }
   });
   
